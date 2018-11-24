853e0e6856ebebb561cff2566bf7207297be53eb
610rodrigo <rodrigo.fla75@gmail.com>
2018-11-24 13:06:23 -0200

    funcao clique adicionada

--- repetir-paragrafo/index.js
+++ repetir-paragrafo/index.js
@@ -1,9 +1,14 @@
 function clique () {
     var repeat = document.getElementsByTagName("p");
     var text = "";
-    for (i = 0; i < 3; i++) {
-        text += document.getElementById("demo").innerHTML = repeat[0].innerHTML;
+    if (repeat == repeat) {
+        for (i = 0; i < 3; i++) {
+            text += document.getElementById("demo").innerHTML = repeat[0].innerHTML;
+        }
+        document.getElementById("demo").innerHTML = text;
     }
-    document.getElementById("demo").innerHTML = text;
+    else {
+        document.getElementById("demo").innerHTML = "nada a exibir";
+    }
 }
 
