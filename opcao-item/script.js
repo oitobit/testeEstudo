358909875b07f1dc8d3bc99b26858c1cba72f229
610rodrigo <rodrigo.fla75@gmail.com>
2018-12-18 18:41:22 -0200

    create opcao-item

---
+++ opcao-item/script.js
@@ -0,0 +1,9 @@
+function openNav () {
+    var cont = document.getElementById("container");
+    if (cont.style.display === "block") {
+        cont.style.display = "none"
+    }
+    else {
+        cont.style.display = "block";
+    }
+}
