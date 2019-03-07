0105b925fe26f51775bddd02db452241287716e3
610rodrigo <rodrigo.fla75@gmail.com>
2019-03-07 12:11:25 -0300

    dom-basic added

---
+++ JS/dom-basic/app.js
@@ -0,0 +1,9 @@
+const h1 = document.getElementById("title");
+
+h1.style.color = "red";
+
+document.getElementById("btn").style.color = "blue";
+
+const style = document.getElementById("btn").style;
+
+console.log(style);
