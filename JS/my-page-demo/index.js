65d24117457898ae35e50b56e03fcd78afbbb010
rodric1 <rodrigo.fla75@gmail.com>
2019-06-04 20:56:28 -0300

    my-page-demo added

---
+++ JS/my-page-demo/index.js
@@ -0,0 +1,12 @@
+var hide = document.getElementById("content");
+var btn = document.getElementById("btn");
+
+function myFunction () {
+    if (hide.style.display == "none") {
+        hide.style.display = "block";
+    }
+
+    else {
+        hide.style.display = "none";
+    }
+}
